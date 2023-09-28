cefcc96e024f015c219f40acf24f6906a676de29
Marius STEPHANY <marius.stephany@gmail.com>
2023-09-28 17:09:56 +0200

    :sparkles: Add control panel

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+import { GUI } from "dat.gui";
 import gsap from "gsap";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
@@ -9,7 +10,22 @@ import {
   shiftKeysBinding,
 } from "./constants";
 
-// Sizes
+// Constants
+const general = {
+  animationDuration: 0.3,
+  isParticlesVisible: true,
+  isMirrorsVisible: true,
+};
+const colors = {
+  background: 0x23323c,
+  particles: 0xffffff,
+  frontFace: 0x32cd32,
+  backFace: 0x0000ff,
+  rightFace: 0xff0000,
+  leftFace: 0xffa500,
+  upFace: 0xffffff,
+  downFace: 0xffff00,
+};
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
@@ -36,16 +52,30 @@ const scene = new THREE.Scene();
 const box = new RoundedBoxGeometry(1, 1, 1, 10, 0.1);
 const colorFace = new THREE.PlaneGeometry(0.8, 0.8);
 const mirrorFace = new THREE.PlaneGeometry(1, 1);
+const particlesGeometry = new THREE.BufferGeometry(1, 32, 32);
+
+// Textures
+const textureLoader = new THREE.TextureLoader();
+const particleTexture = textureLoader.load("/textures/magic.png");
 
 // Materials
-const rightMaterial = new THREE.MeshBasicMaterial({ color: "red" });
-const leftMaterial = new THREE.MeshBasicMaterial({ color: "orange" });
-const upMaterial = new THREE.MeshBasicMaterial({ color: "white" });
-const downMaterial = new THREE.MeshBasicMaterial({ color: "yellow" });
-const frontMaterial = new THREE.MeshBasicMaterial({ color: "limegreen" });
-const backMaterial = new THREE.MeshBasicMaterial({ color: "blue" });
+const rightMaterial = new THREE.MeshBasicMaterial({ color: colors.rightFace });
+const leftMaterial = new THREE.MeshBasicMaterial({ color: colors.leftFace });
+const upMaterial = new THREE.MeshBasicMaterial({ color: colors.upFace });
+const downMaterial = new THREE.MeshBasicMaterial({ color: colors.downFace });
+const frontMaterial = new THREE.MeshBasicMaterial({ color: colors.frontFace });
+const backMaterial = new THREE.MeshBasicMaterial({ color: colors.backFace });
 const backGroundMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.1,
+  sizeAttenuation: true,
+  alphaMap: particleTexture,
+  transparent: true,
+  color: colors.particles,
+  depthWrite: false,
+});
 
+// Cube
 const cubes = [];
 for (let i = 0; i < 27; i++) {
   const cube = new THREE.Group();
@@ -76,6 +106,7 @@ for (let i = 0; i < 27; i++) {
   scene.add(cube);
 }
 
+// Mirrors
 const distance = 4;
 const colorMaterials = [
   leftMaterial,
@@ -85,7 +116,6 @@ const colorMaterials = [
   backMaterial,
   frontMaterial,
 ];
-
 const mirrorPositions = [
   [-distance, 0, 0],
   [distance, 0, 0],
@@ -94,7 +124,6 @@ const mirrorPositions = [
   [0, 0, -distance],
   [0, 0, distance],
 ];
-
 const mirrorFaces = [];
 for (let i = 0; i < 6; i++) {
   const mirror = new THREE.Group();
@@ -113,11 +142,22 @@ for (let i = 0; i < 6; i++) {
   mirror.lookAt(0, 0, 0);
   scene.add(mirror);
 }
-
 mirrorFaces.map((m) => scene.attach(m));
 
+// Particles
+const count = 1000;
+const positions = new Float32Array(count * 3);
+for (let i = 0; i < count * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 15;
+}
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3)
+);
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
+
 let isAnimate = false;
-let general = { animationDuration: 0.3 };
 window.addEventListener("keydown", (e) => {
   if (isAnimate) return;
 
@@ -164,6 +204,7 @@ const canvas = document.querySelector("canvas.webgl");
 const controls = new OrbitControls(camera, canvas);
 
 controls.enablePan = false;
+controls.enableZoom = false;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -172,7 +213,45 @@ const renderer = new THREE.WebGLRenderer({
 renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.setClearColor(0xffffff);
+renderer.setClearColor(colors.background);
+
+// GUI
+const gui = new GUI();
+const generalFolder = gui.addFolder("General");
+generalFolder.add(general, "animationDuration", 0.1, 1, 0.01);
+generalFolder
+  .add(general, "isParticlesVisible")
+  .onChange(() => (particles.visible = !particles.visible));
+generalFolder
+  .add(general, "isMirrorsVisible")
+  .onChange(() =>
+    mirrorFaces.map((mirrorFace) => (mirrorFace.visible = !mirrorFace.visible))
+  );
+const colorsFolder = gui.addFolder("Colors");
+colorsFolder
+  .addColor(colors, "background")
+  .onChange(() => renderer.setClearColor(colors.background));
+colorsFolder
+  .addColor(colors, "particles")
+  .onChange(() => particlesMaterial.color.set(colors.particles));
+colorsFolder
+  .addColor(colors, "frontFace")
+  .onChange(() => frontMaterial.color.set(colors.frontFace));
+colorsFolder
+  .addColor(colors, "backFace")
+  .onChange(() => backMaterial.color.set(colors.backFace));
+colorsFolder
+  .addColor(colors, "rightFace")
+  .onChange(() => rightMaterial.color.set(colors.rightFace));
+colorsFolder
+  .addColor(colors, "leftFace")
+  .onChange(() => leftMaterial.color.set(colors.leftFace));
+colorsFolder
+  .addColor(colors, "upFace")
+  .onChange(() => upMaterial.color.set(colors.upFace));
+colorsFolder
+  .addColor(colors, "downFace")
+  .onChange(() => downMaterial.color.set(colors.downFace));
 
 // Animations
 function tick() {
